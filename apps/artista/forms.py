ad9a1db5e990e6fcbe7663550ffd0d27b65b717e
Diego <kaoxdc@gmail.com>
2021-08-20 13:03:59 -0500

    segunda sesion del proyecto musical

--- apps/artista/forms.py
+++ apps/artista/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.forms import fields
+from django.contrib.auth.models import User
 from .models import *
 
 class agregar_musica_form (forms.ModelForm):
@@ -7,3 +8,38 @@ class agregar_musica_form (forms.ModelForm):
         model = Musica
         fields = '__all__'
         exclude = ['artista',]
+
+class login_form (forms.Form):
+    usuario = forms.CharField(widget=forms.TextInput()) # input TEXT html
+    clave   = forms.CharField(widget=forms.PasswordInput(render_value=False)) # input PASSWROD html
+
+class register_form (forms.Form):
+    username    = forms.CharField(widget= forms.TextInput())
+    email       = forms.EmailField(widget= forms.TextInput())
+    password_1  = forms.CharField(label='password', widget= forms.PasswordInput(render_value= False))
+    password_2  = forms.CharField(label='repite el password', widget= forms.PasswordInput(render_value= False))
+
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        try:
+            u = User.objects.get(username = username)
+        except User.DoesNotExist:
+            return username
+        raise forms.ValidationError('nombre de usuario ya registrado')
+
+    def clean_email (self):
+        email = self.cleaned_data['email']
+        try:
+            u = User.objects.get(email = email)
+        except User.DoesNotExist:
+            return email
+        raise forms.ValidationError ('el correo ya exixte')
+    
+    def clean_password_2 (self):
+        password_1 = self.cleaned_data['password_1']
+        password_2 = self.cleaned_data['password_2']
+        if password_1 == password_2:
+            return password_2
+        else:
+            raise forms.ValidationError ('las claves no coinciden')
+    
--- apps/artista/views.py
+++ apps/artista/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import redirect, render
+from django.contrib.auth import login, logout, authenticate
 from .models import *
 from .forms import *
 # Create your views here.
@@ -10,12 +11,42 @@ def canciones_view (request):
 ''' seccion de autenticacion '''
 
 def login_view (request):
+    usu = ""
+    cla = ""
+    if request.method == 'POST':
+        formulario = login_form(request.POST)
+        if formulario.is_valid():
+            usu = formulario.cleaned_data['usuario']
+            cla = formulario.cleaned_data['clave']
+            usuario = authenticate(username=usu, password=cla)
+            if usuario is not None and usuario.is_active:
+                login(request, usuario)
+                return redirect('/')
+            else: 
+                msj = 'usuario o clave incorrectos'
+    
+    formulario = login_form()
     return render(request, 'artista/login.html', locals())
 
 def logout_view (request):
-    return render(request, 'artista/logout.html', locals())
+    logout(request)
+    return redirect ('/login/')
 
 def register_view (request):
+
+    formulario = register_form()
+    if request.method == 'POST':
+        formulario = register_form(request.POST)
+        if formulario.is_valid():
+            usuario = formulario.cleaned_data['username']
+            correo = formulario.cleaned_data['email']
+            password_1 = formulario.cleaned_data['password_1']
+            password_2 = formulario.cleaned_data['password_2']
+            u = User.objects.create_user(username= usuario, email= correo, password= password_2, is_superuser=True, is_staff=True)
+            u.save()
+            return redirect('/login/')
+        else:
+            return render(request, 'artista/register.html', locals())        
     return render(request, 'artista/register.html', locals())
 
 ''' Seccion del artista '''
